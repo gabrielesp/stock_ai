cd2c50d3bda05a1aee2533caba3d0aeb7fa58b46
albertoesmp <albertoesmp@gmail.com>
2019-08-05 15:43:50 +0200

    Added plot for calculated mu and sigma with data from path to geometric brownian custom test

--- src/tests/custom/test_geometric_brownian.py
+++ src/tests/custom/test_geometric_brownian.py
@@ -1,34 +1,52 @@
+import sys
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from models.geometric_brownian import GeometricBrownianMotion
 
 
 def generateTestData(n=10):
-    return [
+    return list(enumerate([
         x*100+1000 for x in np.random.rand(n).tolist()
-    ]
+    ]))
+
+
+def obtainData(path=None, n=10):
+    if path is None:
+        return generateTestData(n=n)
+
+    vals = pd.read_csv(path, delimiter='\t')['Low']\
+        .dropna(axis=0)\
+        .to_list()
+    print(vals)
+    n = len(vals)
+    return [[t, vals[t]] for t in range(n)]
 
 
 # ---  M A I N  --- #
 # ----------------- #
 if __name__ == '__main__':
     # Prepare
-    deep = 10
+    deep = 100
     n = 100
-    data = generateTestData(n=n)
+    if len(sys.argv) > 1:
+        data = obtainData(path=sys.argv[1])
+        n = len(data)
+    else:
+        data = generateTestData(n=n)
     # estimator = GeometricBrownianMotion(
     #    mu=np.log(np.mean(data)),
     #    sigma=np.log(np.std(data))
     # )
     estimator = GeometricBrownianMotion(
-        mu=1.0,
-        sigma=1.4
+        mu=0.00198790113187,
+        sigma=0.009181598109527
     )
 
     # Calculate
     estimations = [estimator.calc(
-        data0=data[0],
+        data0=data[0][1],
         n=n,
         seed=np.random.randint(0, 1337)
     ) for i in range(deep)]
@@ -36,7 +54,7 @@ if __name__ == '__main__':
     # Plot
     print(data)
     # plt.plot(range(n), estimations)
-    plt.scatter(range(n), data)
+    plt.scatter(range(n), [v for [t,v] in data])
     for estimation in estimations:
         # plt.scatter(range(n), estimation)
         plt.plot(range(n), estimation)
